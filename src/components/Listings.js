0289f1348fb1b8ca221657a07285ddd66e52e7f3
rlly92 <reubenlimliangyi@gmail.com>
2023-06-10 21:21:35 +0800

    firebase checked again and adjusted to be in sync with .env file. Items CSS block removed in Listings.js

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -30,14 +30,6 @@ import { red } from "@mui/material/colors";
 
 import "./ListingsStyle.css";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 const Listings = () => {
   const { logout, isAuthenticated, getAccessTokenSilently, user, isLoading } =
     useAuth0();
