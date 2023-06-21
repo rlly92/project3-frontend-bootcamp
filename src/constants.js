15413504430d1c8a51a9147058c42e59d12cd487
rlly92 <reubenlimliangyi@gmail.com>
2023-06-21 15:45:06 +0800

    env updated

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://long-dawn-7576.fly.dev"
+    : "http://localhost:3000";
